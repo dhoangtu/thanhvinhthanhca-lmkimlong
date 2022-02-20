ce14ebb0ed3228c34240602fe75ddced327c074b
dhoangtu <dhoangtu@gmail.com>
2022-02-20 11:18:04 +0700

    xuat ban

--- sach/in-nhanh.bat
+++ sach/in-nhanh.bat
@@ -8,4 +8,6 @@ pdflatex so-trang-chan-le.tex
 
 D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc-a5.pdf bia-trong-a5.pdf so-trang-chan-le.pdf muc-luc.pdf blank-a5.pdf bia-sau-a5.pdf -- thanhvinhthanhca-lmkimlong.pdf
 
+D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-trong-a5.pdf so-trang-chan-le.pdf muc-luc.pdf blank-a5.pdf -- in-sach.pdf
+
 del /s /f /q %GEN% nhac.pdf *.aux *.log so-trang-chan-le.pdf
